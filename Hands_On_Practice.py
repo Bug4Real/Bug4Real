dae18353bd9ce5cc6944e687c935a752bd7758f0
Bug4Real <iggauravofficial@gmail.com>
2024-11-27 20:56:07 +0530

    Update

---
+++ Hands_On_Practice.py
@@ -0,0 +1,8 @@
+#Q1: Calculate 3% discount on the amount entered by the user and display the output as:
+The discount on <<amount>> is <<amount after discount>>
+
+#Code:
+amt = int(input("Enter the amount: "))
+dis = amt/100*3
+amt_dis = amt-dis
+print("The Discount on", amt, "is", amt_dis)
